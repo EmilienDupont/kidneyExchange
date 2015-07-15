254687ffafb330c6d16599a223494e9b65064520
EmilienDupont <emilien.dupont@ymail.com>
2015-07-15 15:13:46 -0700

    added visualisation, gurobi and d3 connect with correct data structures

--- kidneyexchange.py
+++ kidneyexchange.py
@@ -2,6 +2,7 @@
 
 from gurobipy import *
 
+# Note that these are in incorrect format
 vertices  = range(5)
 edges = { (0,1) : 1, (1,0) : 1, (0,2) : 1, (2,0) : 1,
           (0,4) : 1, (4,0) : 1, (1,4) : 1, (4,1) : 1,
@@ -38,40 +39,55 @@ def threeCycle(vertices, edges):
                 threeCycles[(u,w,v)] = edges[(u,v)] + edges[(u,w)] + edges[(w,v)]
     return threeCycles
 
-twoCycles = twoCycle(vertices,edges)
-threeCycles = threeCycle(vertices,edges)
+def optimize(vertices, edges):
+    twoCycles = twoCycle(vertices,edges)
+    threeCycles = threeCycle(vertices,edges)
 
-for cycle in twoCycles:
-    print cycle
+    m = Model()
 
-for cycle in threeCycles:
-    print cycle
+    c = {}
 
-m = Model()
+    for cycle in twoCycles:
+        c[cycle] = m.addVar(vtype=GRB.BINARY, name="c_%s" % str(cycle))
 
-c = {}
+    for cycle in threeCycles:
+        c[cycle] = m.addVar(vtype=GRB.BINARY, name="c_%s" % str(cycle))
 
-for cycle in twoCycles:
-    c[cycle] = m.addVar(vtype=GRB.BINARY, name="c_%s" % str(cycle))
+    m.update()
 
-for cycle in threeCycles:
-    c[cycle] = m.addVar(vtype=GRB.BINARY, name="c_%s" % str(cycle))
+    for v in vertices:
+        constraint = 0
+        for cycle in c:
+            if (v in cycle):
+                constraint = constraint + c[cycle]
+        m.addConstr( constraint <= 1 , name="v%d" % v)
 
-m.update()
+    m.setObjective( quicksum( c[cycle] * twoCycles[cycle] for cycle in twoCycles ) +
+                    quicksum( c[cycle] * threeCycles[cycle] for cycle in threeCycles ),
+                    GRB.MAXIMIZE )
 
-for v in vertices:
-    constraint = 0
+    m.optimize()
+
+    solution = []
+
     for cycle in c:
-        if (v in cycle):
-            constraint = constraint + c[cycle]
-    m.addConstr( constraint <= 1 , name="v%d" % v)
+        if (c[cycle].X > .5):
+            solution.append(cycle)
 
-m.setObjective( quicksum( c[cycle] * twoCycles[cycle] for cycle in twoCycles ) +
-                quicksum( c[cycle] * threeCycles[cycle] for cycle in threeCycles ),
-                GRB.MAXIMIZE )
+    return solution
 
-m.optimize()
+# Because javascript does not have tuples, need to change data structures
+# We receive edges as {edgeweight: list of edges (as arrays)} and want to transform
+# this to {edge (as tuple): edgeweight}
+def transform(nodes, edges):
+    newNodes = range(len(nodes))
+    newEdges = {}
+    for edgeweight in edges:
+        for edge in edges[edgeweight]:
+            newEdges[(edge[0], edge[1])] = float(edgeweight)
+    print newEdges
+    return optimize(newNodes, newEdges)
 
-for v in m.getVars():
-    if (v.X):
-        print v
+solution = optimize(vertices, edges)
+
+print solution
